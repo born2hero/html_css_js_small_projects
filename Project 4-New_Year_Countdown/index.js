c1bb03d0430da37c9346c87de6a219d383b4f303
Abhishek Maurya <m1999abhishek@gmail.com>
2022-12-06 15:04:47 +0530

    Add files via upload

---
+++ Project 4-New_Year_Countdown/index.js
@@ -0,0 +1,40 @@
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+
+const newYearTime = new Date("Jan 1,2023 00:00:00").getTime();
+
+updateCountdown();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  // console.log(now,newYearTime);
+
+  const gap = newYearTime - now;
+  // console.log(gap);
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const remainingDay = Math.floor(gap / day);
+  //   console.log(remainingDay);
+
+  const remainingHour = Math.floor((gap % day) / hour);
+  //   console.log(remainingHour);
+
+  const remainingMinute = Math.floor((gap % hour) / minute);
+  //   console.log(remainingMinute);
+
+  const remainingSecond = Math.floor((gap % minute) / second);
+  //   console.log(remainingSecond);
+
+  dayEl.innerText = remainingDay;
+  hourEl.innerText = remainingHour;
+  minuteEl.innerText = remainingMinute;
+  secondEl.innerText = remainingSecond;
+
+  setTimeout(updateCountdown, 1000);
+}
